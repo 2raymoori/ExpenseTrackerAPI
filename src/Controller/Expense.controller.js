ab1b0a1455aa36a639fb7e5b7652d08d72d0fe02
2raymoori <lo2raymoori@gmail.com>
2023-01-28 15:38:15 +0100

    A Commit for expense endpoint Test

--- src/Controller/Expense.controller.js
+++ src/Controller/Expense.controller.js
@@ -1,116 +1,159 @@
 // const { ObjectId } = require('mongoose');
-const { ObjectId } = require('mongodb').ObjectId;
-const ExpenseModel = require('../Model/Expense.model');
+const { ObjectId } = require("mongodb").ObjectId;
+const ExpenseModel = require("../Model/Expense.model");
 
-const addExpense = async(req,res)=>{
-    try {
-        const {name,amount,expenseDate,expenseCategory,description} = req.body;
-        const newExpense = new ExpenseModel();
-        newExpense.name = name;
-        if(amount > 0){
-            newExpense.amount = amount
-        }else{
-            return res.status(201).send({"status":"error","data":"The Amount for Expense cannot be or less than 0."})
-        }
-        newExpense.expenseCategory = expenseCategory;
-        if(expenseDate){
-            newExpense.expenseDate = expenseDate
-        }
-        if(description){
-            newExpense.description = description;
-        }
-        newExpense.user = req.user.id;
-        await newExpense.save();
-        res.status(200).send({"status":"Success","data":newExpense});
-    } catch (error) {
-        res.status(500).send({"status":"Error","data":error.message})
+const addExpense = async (req, res) => {
+  try {
+    const { name, amount, expenseDate, expenseCategory, description } =
+      req.body;
+    const newExpense = new ExpenseModel();
+    if (amount > 0) {
+      newExpense.amount = amount;
+    } else {
+      return res.status(201).send({
+        status: "error",
+        data: "The Amount for Expense cannot be or less than 0.",
+      });
     }
-}
-const updateExpense = async(req,res)=>{
-    try {
-        const {id} = req.params;
-        const curUser = req.user.id;
-        const {name,amount,expenseDate,expenseCategory} = req.body;
-        const expenseToUpdate = await ExpenseModel.findByIdAndUpdate({"_id":id,user:ObjectId(curUser)});
-        if(expenseToUpdate){
-            if(name){
-                expenseToUpdate.name = name;
-            }
-            if(amount){
-                if(amount > 0){
-                    newExpense.amount = amount
-                }else{
-                    return res.status(201).send({"status":"error","data":"The Amount for Expense cannot be or less than 0."})
-                }
-        
-            }
-            if(expenseCategory){
-                expenseToUpdate.expenseCategory = expenseToUpdate;
-            }
-            if(expenseDate){
-                expenseToUpdate.expenseDate = expenseDate;
-            }
-            console.log(expenseToUpdate);
-            await expenseToUpdate.save();
-            return res.status(200).send({"status":"success","data":expenseToUpdate});
-        }else{
-            return res.status(201).send({"status":"error","data":"Sorry There is no such Expense with this id."})
-        }
-    } catch (error) {
-        if(error.kind ==="ObjectId"){
-            return res.status(201).send({"status":"error","data":"Sorry There is no such Expense with this id."})
-        }
-        return res.status(500).send({"status":"error","data":error.message})
+    newExpense.expenseCategory = expenseCategory;
+    if (expenseDate) {
+      newExpense.expenseDate = expenseDate;
     }
-}
-const deleteExpense = async(req,res)=>{
-    try {
-        const {id} = req.params;
-        const curUser = req.user.id;
-        const expenseToUpdate = await ExpenseModel.findByIdAndDelete({"_id":id,user:ObjectId(curUser)});
-        if(expenseToUpdate){
-            await expenseToUpdate.delete();
-            res.status(200).send({"status":"success","data":expenseToUpdate});
-        }else{
-            res.status(201).send({"status":"error","data":"Sorry There is no such Expense with this id."})
-        }
-    } catch (error) {
-        if(error.kind ==="ObjectId"){
-            res.status(201).send({"status":"error","data":"Sorry There is no such Expense with this id."})
-        }
-        res.status(500).send({"status":"error","data":error.message})
+    if (description) {
+      newExpense.description = description;
     }
-}
-const allExpense = async(req,res)=>{
-    try {
-        const curUser = req.user.id;
-        const allExpense = await ExpenseModel.find({user:ObjectId(curUser)}).populate("expenseCategory",["name"]);
-        res.status(200).send({"status":"Success","data":allExpense})
-    } catch (error) {
-        res.status(500).send({"status":"error","data":error.message})
-    }
-}
-const expenseByid = async(req,res)=>{
-    try{
-        const {id} = req.params;
-        const curUser = req.user.id;
-        const currentExpense = await ExpenseModel.findById({"_id":id,user:ObjectId(curUser)}).populate("expenseCategory",["name"]);
-        if(currentExpense){
-            res.status(200).send({"status":"Success","data":currentExpense})
-        }else{
-            res.status(201).send({"status":"error","data":"Sorry There is no such Expense with this id."})
+    newExpense.name = name;
+    newExpense.user = req.user.id;
+    await newExpense.save();
+    res.status(200).send({ status: "Success", data: newExpense });
+  } catch (error) {
+    res.status(500).send({ status: "Error", data: error.message });
+  }
+};
+const updateExpense = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const curUser = req.user.id;
+    const { name, amount, expenseDate, description, expenseCategory } =
+      req.body;
+    const expenseToUpdate = await ExpenseModel.findByIdAndUpdate({
+      _id: id,
+      user: curUser,
+    });
+    if (expenseToUpdate) {
+      if (name) {
+        expenseToUpdate.name = name;
+      }
+      if (amount) {
+        if (amount > 0) {
+          expenseToUpdate.amount = amount;
+        } else {
+          return res.status(201).send({
+            status: "error",
+            data: "The Amount for Expense cannot be or less than 0.",
+          });
         }
-    }catch (error) {
-        if(error.kind ==="ObjectId"){
-            res.status(201).send({"status":"error","data":"Sorry There is no such Expense with this id."})
-        }
-        res.status(500).send({"status":"error","data":error.message})
+      }
+      if (expenseCategory) {
+        expenseToUpdate.expenseCategory = expenseCategory;
+      }
+      if (expenseDate) {
+        expenseToUpdate.expenseDate = expenseDate;
+      }
+      if (description) {
+        expenseToUpdate.description = description;
+      }
+      console.log(expenseToUpdate);
+      await expenseToUpdate.save();
+      return res.status(200).send({ status: "success", data: expenseToUpdate });
+    } else {
+      return res.status(201).send({
+        status: "error",
+        data: "Sorry There is no such Expense with this id.",
+      });
     }
-}
+  } catch (error) {
+    if (error.kind === "ObjectId") {
+      return res.status(201).send({
+        status: "error",
+        data: "Sorry There is no such Expense with this id.",
+      });
+    }
+    return res.status(500).send({ status: "error", data: error.message });
+  }
+};
+const deleteExpense = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const curUser = req.user.id;
+    const expenseToUpdate = await ExpenseModel.findByIdAndDelete({
+      _id: id,
+      user: curUser,
+    });
+    if (expenseToUpdate) {
+      await expenseToUpdate.delete();
+      res.status(200).send({ status: "success", data: expenseToUpdate });
+    } else {
+      res.status(201).send({
+        status: "error",
+        data: "Sorry There is no such Expense with this id.",
+      });
+    }
+  } catch (error) {
+    if (error.kind === "ObjectId") {
+      res.status(201).send({
+        status: "error",
+        data: "Sorry There is no such Expense with this id.",
+      });
+    }
+    res.status(500).send({ status: "error", data: error.message });
+  }
+};
+const allExpense = async (req, res) => {
+  try {
+    const curUser = req.user.id;
+    const allExpense = await ExpenseModel.find({
+      user: curUser,
+    })
+      .populate("expenseCategory", ["name"])
+      .populate("user", ["email", "fName"]);
+    res.status(200).send({ status: "Success", data: allExpense });
+  } catch (error) {
+    res.status(500).send({ status: "error", data: error.message });
+  }
+};
+const expenseByid = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const curUser = req.user.id;
+    const currentExpense = await ExpenseModel.findById({
+      _id: id,
+      user: curUser,
+    })
+      .populate("expenseCategory", ["name"])
+      .populate("user", ["email", "fName"]);
+    if (currentExpense) {
+      return res.status(200).send({ status: "Success", data: currentExpense });
+    } else {
+      return res.status(201).send({
+        status: "error",
+        data: "Sorry There is no such Expense with this id.",
+      });
+    }
+  } catch (error) {
+    if (error.kind === "ObjectId") {
+      res.status(201).send({
+        status: "error",
+        data: "Sorry There is no such Expense with this id.",
+      });
+    }
+    res.status(500).send({ status: "error", data: error.message });
+  }
+};
 module.exports = {
-    addExpense,
-    updateExpense,
-    deleteExpense,
-    allExpense,
-    expenseByid
-}
+  addExpense,
+  updateExpense,
+  deleteExpense,
+  allExpense,
+  expenseByid,
+};
